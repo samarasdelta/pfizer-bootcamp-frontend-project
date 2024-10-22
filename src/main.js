4dd28647f24d5f7516836ac65164cb0a78bdb41b
zummashu <samarasdelta@gmail.com>
2024-10-22 20:37:14 +0300

    change to vite app

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+// import State from './State.vue'
+// import DirectivesBuiltIn from './DirectivesBuiltIn.vue'
+import MethodsEvents from './MethodsEvents.vue'
+
+
+createApp(App).mount('#app')
+
+// createApp(State).mount('#app');
+
+// createApp(DirectivesBuiltIn).mount('#app');
+
+// createApp(MethodsEvents).mount('#app');
+
